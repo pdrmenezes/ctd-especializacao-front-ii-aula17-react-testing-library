ab1321c113466bee11764a1f0cc406141725f7e8
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-08-17 22:08:05 -0300

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  setupFilesAfterEnv: ['./jest.setupAfterEnv.ts'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  moduleNameMapper: {
+    '^features/(.*)$': '<rootDir>/src/features/$1',
+    '^data/(.*)$': '<rootDir>/src/data/$1',
+    '^pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^store/(.*)$': '<rootDir>/src/store/$1',
+    '^test/(.*)$': '<rootDir>/src/test/$1'
+  },
+  testPathIgnorePatterns: ['index.ts'],
+  collectCoverageFrom: [
+    'src/features/**/*.ts',
+    'src/features/**/*.tsx',
+    'src/pages/**/*.ts',
+    'src/pages/**/*.tsx'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50
+    }
+  }
+};
